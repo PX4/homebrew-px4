be3ed3c7858bc3fa08fa3d8ab8a7738baa722b21
px4dev <px4@purgatory.org>
2013-05-31 13:52:41 +0200

    Add an experimental formula for the ARM-built gcc-4.7.  We saw problems with earlier 4.7 releases, but this one doesn't seem to be having the same major issues, so it's worth experimenting with.

---
+++ gcc-arm-none-eabi-47.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class GccArmNoneEabi < Formula
+  homepage 'https://launchpad.net/gcc-arm-embdded'
+  version '20130313'
+  url 'https://launchpadlibrarian.net/135590305/gcc-arm-none-eabi-4_7-2013q1-20130313-mac.tar.bz2'
+  sha1 '97ef3b02ef277c03c956add1bea9ecb80a3e3ad2'
+
+  def install 
+    ohai 'Copying binaries...'
+    system 'cp', '-rv', 'arm-none-eabi', 'bin', 'lib', 'share', "#{prefix}/"
+  end
+end
