65be1d599694c775b5efaeaf32f8cde8834ae267
Lorenz Meier <lm@inf.ethz.ch>
2014-04-09 16:51:22 +0200

    Removed requirement for https

--- gcc-arm-none-eabi.rb
+++ gcc-arm-none-eabi.rb
@@ -3,7 +3,7 @@ require 'formula'
 class GccArmNoneEabi < Formula
   homepage 'https://launchpad.net/gcc-arm-embdded'
   version '20130916'
-  url 'https://launchpadlibrarian.net/151487551/gcc-arm-none-eabi-4_7-2013q3-20130916-mac.tar.bz2'
+  url 'http://launchpadlibrarian.net/151487551/gcc-arm-none-eabi-4_7-2013q3-20130916-mac.tar.bz2'
   sha1 'a29eda3d4351bfe47749a242f6faa7cbd630d28b'
 
   def install 
