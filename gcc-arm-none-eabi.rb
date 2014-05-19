08bd3f602045bb17b5e6be0dfde7e462912b3943
Lorenz Meier <lm@inf.ethz.ch>
2014-05-19 10:22:42 +0200

    Fix download link

--- gcc-arm-none-eabi.rb
+++ gcc-arm-none-eabi.rb
@@ -3,7 +3,7 @@ require 'formula'
 class GccArmNoneEabi < Formula
   homepage 'https://launchpad.net/gcc-arm-embdded'
   version '20140408'
-  url 'http://launchpadlibrarian.net/174121628/gcc-arm-none-eabi-4_7-2014q2-20140408-linux.tar.bz2'
+  url 'http://launchpadlibrarian.net/174121504/gcc-arm-none-eabi-4_7-2014q2-20140408-mac.tar.bz2'
   sha1 '7b15f1f27e4801d1b4fed8b1a90e7481e11fd7a2'
 
   def install 
