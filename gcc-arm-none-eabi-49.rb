d66300488e8fef732761cfc6f18742a7e51e7fc9
Charles Korn <me@charleskorn.com>
2016-04-10 15:13:46 +1000

    Fix homepage URLs

--- gcc-arm-none-eabi-49.rb
+++ gcc-arm-none-eabi-49.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class GccArmNoneEabi49 < Formula
-  homepage 'https://launchpad.net/gcc-arm-embdded'
+  homepage 'https://launchpad.net/gcc-arm-embedded'
   version '20150609'
   url 'https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q2-update/+download/gcc-arm-none-eabi-4_9-2015q2-20150609-mac.tar.bz2'
   sha1 '7f2d8c3644cd76da1b78d17a0770c8c0efbff957'
