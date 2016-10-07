7e806bffce79052a39d1f68b2a9bc48c1735c823
Jake Rockland <jakerockland@gmail.com>
2016-10-06 20:11:53 -0700

    Update gcc-arm-none-eabi.rb

--- gcc-arm-none-eabi.rb
+++ gcc-arm-none-eabi.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class GccArmNoneEabi54 < Formula
+class GccArmNoneEabi < Formula
   homepage 'https://launchpad.net/gcc-arm-embedded'
   version '20160926'
   url 'https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q3-update/+download/gcc-arm-none-eabi-5_4-2016q3-20160926-mac.tar.bz2'
