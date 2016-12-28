71c3f78151d4194b337393ad27b6907e70cb4a36
Lorenz Meier <lorenz@px4.io>
2016-12-28 11:32:24 +0100

    Keep the current version still as active packet, even though the default has changed

---
+++ gcc-arm-none-eabi-54.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class GccArmNoneEabi < Formula
+  homepage 'https://developer.arm.com/open-source/gnu-toolchain/gnu-rm/downloads'
+  version '20160928'
+  #url 'https://armkeil.blob.core.windows.net/developer//sitecore/shell/-/media/Files/downloads/gnu-rm/5_4-2016q3/gcc-arm-none-eabi-5_4-2016q3-20160926-mac,-d-,tar.bz2'
+  url 'http://px4-travis.s3.amazonaws.com/toolchain/gcc-arm-none-eabi-5_4-2016q3-20160926-mac-d.tar.bz2'
+  sha256 '5656cdec40f99d5c054a85bbc694276e1c4a1488cdacbbc448bc6acd3bbe070d'
+
+  def install
+    ohai 'Copying binaries...'
+    system 'cp', '-r', 'arm-none-eabi', 'bin', 'lib', 'share', "#{prefix}/"
+  end
+end
