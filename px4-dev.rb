89857931b23bbf0b6385a86997139fdc47e81b46
Lorenz Meier <lorenz@px4.io>
2017-07-10 18:38:05 +0200

    Adjust install step to new file name

--- px4-dev.rb
+++ px4-dev.rb
@@ -14,12 +14,12 @@ class Px4Dev < Formula
 
 	homepage 'http://px4.io'
 	version '1.6.0.3'
-	url 'https://raw.githubusercontent.com/PX4/Firmware/master/Tools/px4'
+	url 'https://raw.githubusercontent.com/PX4/Firmware/master/Tools/px4.py'
 	sha256 '48199ee9ff392eff8a1efcd177e2c10f4a4dfd9877e52e13b1f3540d5dfedac9'
 
 	def install
 		system 'mkdir', '-p', "#{prefix}/bin/"
-		system 'cp', 'px4', "#{prefix}/bin/"
+		system 'cp', 'px4.py', "#{prefix}/bin/"
 		ohai 'PX4 Toolchain Installed'
 	end
 end
