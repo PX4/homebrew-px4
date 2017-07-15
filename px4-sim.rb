1e889bb0ba3e1c3f48762a0a3d005e45e4cbc2ae
Lorenz Meier <lorenz@px4.io>
2017-07-15 23:05:54 +0200

    Fix PX4 sim

--- px4-sim.rb
+++ px4-sim.rb
@@ -10,12 +10,12 @@ class Px4Sim < Formula
 
 	homepage 'http://px4.io'
 	version '1.6.0.1'
-	url 'https://raw.githubusercontent.com/PX4/Firmware/master/Tools/px4'
+	url 'https://raw.githubusercontent.com/PX4/Firmware/master/Tools/px4.py'
 	sha256 '48199ee9ff392eff8a1efcd177e2c10f4a4dfd9877e52e13b1f3540d5dfedac9'
 
 	def install
 		system 'mkdir', '-p', "#{prefix}/bin/"
-		system 'cp', 'px4', "#{prefix}/bin/px4-sim"
+		system 'cp', 'px4.py', "#{prefix}/bin/px4-sim.py"
 		ohai 'PX4 Simulation Installed'
 	end
 end
