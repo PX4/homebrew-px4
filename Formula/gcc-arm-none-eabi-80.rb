8e09968c3efaf86fb6b2a8a24f00467842a28a7f
Lorenz Meier <lorenz@px4.io>
2019-01-19 21:06:01 +0100

    Add recipe for GCC 8.0

---
+++ Formula/gcc-arm-none-eabi-80.rb
@@ -0,0 +1,14 @@
+require "formula"
+
+class GccArmNoneEabi80 < Formula
+  desc "GNU Embedded Toolchain for ARM"
+  homepage "https://developer.arm.com/open-source/gnu-toolchain/gnu-rm/downloads"
+  url "https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/8-2018q4/gcc-arm-none-eabi-8-2018-q4-major-mac.tar.bz2"
+  version "20181220"
+  sha256 "0b528ed24db9f0fa39e5efdae9bcfc56bf9e07555cb267c70ff3fee84ec98460"
+
+  def install
+    ohai "Copying binaries..."
+    cp_r %w[arm-none-eabi bin lib share], "#{prefix}/"
+  end
+end
