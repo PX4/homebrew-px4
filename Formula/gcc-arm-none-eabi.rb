73043efdc4aee21327bf1051193d1f286220437c
Lorenz Meier <lorenz@px4.io>
2019-01-19 21:02:45 +0100

    Upgrading default GCC version (7.2) to latest

--- Formula/gcc-arm-none-eabi.rb
+++ Formula/gcc-arm-none-eabi.rb
@@ -3,9 +3,9 @@ require "formula"
 class GccArmNoneEabi < Formula
   desc "GNU Embedded Toolchain for ARM"
   homepage "https://developer.arm.com/open-source/gnu-toolchain/gnu-rm/downloads"
-  url "https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/7-2017q4/gcc-arm-none-eabi-7-2017-q4-major-mac.tar.bz2"
-  version "20171218"
-  sha256 "89b776c7cf0591c810b5b60067e4dc113b5b71bc50084a536e71b894a97fdccb"
+  url "https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/7-2018q2/gcc-arm-none-eabi-7-2018-q2-update-mac.tar.bz2"
+  version "20180627"
+  sha256 "c1c4af5226d52bd1b688cf1bd78f60eeea53b19fb337ef1df4380d752ba88759"
 
   def install
     ohai "Copying binaries..."
