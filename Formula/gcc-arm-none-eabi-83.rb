1a9cad83385ee795c41c216a2542686e0d14c577
Drashna Jaelre <drashna@live.com>
2019-09-18 12:24:39 -0700

    fix link for gcc-arm-none-eabi-83

--- Formula/gcc-arm-none-eabi-83.rb
+++ Formula/gcc-arm-none-eabi-83.rb
@@ -3,7 +3,7 @@ require "formula"
 class GccArmNoneEabi83 < Formula
   desc "GNU Embedded Toolchain for ARM"
   homepage "https://developer.arm.com/open-source/gnu-toolchain/gnu-rm/downloads"
-  url "https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/gccarmnoneeabi532016q120160330mactar.bz2"
+  url "https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/8-2019q3/RC1.1/gcc-arm-none-eabi-8-2019-q3-update-mac.tar.bz2"
   version "20190709"
   sha256 "fc235ce853bf3bceba46eff4b95764c5935ca07fc4998762ef5e5b7d05f37085"
 
