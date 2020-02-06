2a50239494c3614078f7412dfa6ec447bc21caa9
Julian Oes <julian@oes.ch>
2020-02-06 17:47:05 +0100

    Formula: try one more way to install Java 8

--- Formula/px4-sim-jmavsim.rb
+++ Formula/px4-sim-jmavsim.rb
@@ -7,7 +7,7 @@ class Px4SimJmavsim < Formula
   version "1.6.5.0"
   sha256 "48199ee9ff392eff8a1efcd177e2c10f4a4dfd9877e52e13b1f3540d5dfedac9"
   depends_on "ant"
-  depends_on :adoptopenjdk/openjdk/adoptopenjdk8
+  depends_on :adoptopenjdk8
 
   def install
     mkdir_p "#{bin}/"
