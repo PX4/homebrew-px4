2a63edffd67fb0bdbfd956c2bf4631b41d38206d
Julian Oes <julian@oes.ch>
2020-02-07 07:15:03 +0100

    Formula: remove cask dependency, add comment

--- Formula/px4-sim-jmavsim.rb
+++ Formula/px4-sim-jmavsim.rb
@@ -1,5 +1,9 @@
 require "formula"
 
+# This depends on AdoptOpenJdk 8 installed using:
+# brew tap adoptopenjdk/openjdk
+# brew cask install adoptopenjdk8
+
 class Px4SimJmavsim < Formula
   desc "PX4 jMAVSim simulation"
   homepage "http://px4.io"
@@ -7,7 +11,6 @@ class Px4SimJmavsim < Formula
   version "1.6.5.0"
   sha256 "48199ee9ff392eff8a1efcd177e2c10f4a4dfd9877e52e13b1f3540d5dfedac9"
   depends_on "ant"
-  depends_on :adoptopenjdk8
 
   def install
     mkdir_p "#{bin}/"
