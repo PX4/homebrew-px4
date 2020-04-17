ba4102591741dc48ab0dbdb07f71fdebb62960ed
TSC21 <n.marques21@hotmail.com>
2020-04-17 12:19:57 +0100

    formula: gcc-arm-none-eabi: update to use GNU Arm Embedded Toolchain Version 9

--- Formula/gcc-arm-none-eabi.rb
+++ Formula/gcc-arm-none-eabi.rb
@@ -3,9 +3,9 @@ require "formula"
 class GccArmNoneEabi < Formula
   desc "GNU Embedded Toolchain for ARM"
   homepage "https://developer.arm.com/open-source/gnu-toolchain/gnu-rm/downloads"
-  url "https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/7-2018q2/gcc-arm-none-eabi-7-2018-q2-update-mac.tar.bz2"
-  version "20180627"
-  sha256 "c1c4af5226d52bd1b688cf1bd78f60eeea53b19fb337ef1df4380d752ba88759"
+  url "https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2019q4/RC2.1/gcc-arm-none-eabi-9-2019-q4-major-mac.tar.bz2"
+  version '9-2019-q4-major'
+  sha256 "1249f860d4155d9c3ba8f30c19e7a88c5047923cea17e0d08e633f12408f01f0"
 
   def install
     ohai "Copying binaries..."
