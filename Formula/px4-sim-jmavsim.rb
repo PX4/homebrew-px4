f0328499c176dcf1862f8090d1bfc17fd60c1bf0
Jan Kaiser <jan.kaiser.email@googlemail.com>
2020-05-04 15:05:35 +0200

    Fixed SHA256 for px4-sim recipes

--- Formula/px4-sim-jmavsim.rb
+++ Formula/px4-sim-jmavsim.rb
@@ -9,7 +9,7 @@ class Px4SimJmavsim < Formula
   homepage "http://px4.io"
   url "https://raw.githubusercontent.com/PX4/Firmware/master/Tools/px4.py"
   version "1.6.5.0"
-  sha256 "48199ee9ff392eff8a1efcd177e2c10f4a4dfd9877e52e13b1f3540d5dfedac9"
+  sha256 "dafe6a32d270c48cbd42960adb2155bd5f60d1aec8cb644b10f8cafc0c8d2e8f"
   depends_on "ant"
   depends_on "px4-dev"
 
