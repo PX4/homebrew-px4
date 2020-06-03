36c715c8ab0d35a03f353055584cdcd28e9a1aa2
pietrodn <powerpdn@gmail.com>
2020-06-03 23:46:46 +0200

    fix: style error and new make target

--- Formula/genromfs.rb
+++ Formula/genromfs.rb
@@ -1,7 +1,7 @@
 require "formula"
 
 class Genromfs < Formula
-  desc "tool for creating romfs images"
+  desc "Tool for creating romfs images"
   homepage "https://github.com/chexum/genromfs"
   url "http://px4-travis.s3.amazonaws.com/toolchain/genromfs-0.5.2.tar.gz"
   sha256 "30f37fc734572c1dbaa2504585bc23ba6b8fd7df767ae7155995b2ca0ebed960"
