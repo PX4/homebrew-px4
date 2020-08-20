76ccee70e0ebba52d341275deac01334736c530f
TSC21 <n.marques21@hotmail.com>
2020-08-20 13:48:09 +0200

    gcc-arm-none-eabi: udpate to version 9-2020q2

--- Formula/gcc-arm-none-eabi.rb
+++ Formula/gcc-arm-none-eabi.rb
@@ -3,7 +3,7 @@ require "formula"
 class GccArmNoneEabi < Formula
   desc "GNU Embedded Toolchain for ARM"
   homepage "https://developer.arm.com/open-source/gnu-toolchain/gnu-rm/downloads"
-  url "https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2019q4/RC2.1/gcc-arm-none-eabi-9-2019-q4-major-mac.tar.bz2"
+  url "https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2020q2/gcc-arm-none-eabi-9-2020-q2-update-mac.tar.bz2"
   version "20191106"
   sha256 "1249f860d4155d9c3ba8f30c19e7a88c5047923cea17e0d08e633f12408f01f0"
 
