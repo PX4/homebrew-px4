1c689c5dbbc6cee8e063cffcd194594585e1cc7b
TSC21 <n.marques21@hotmail.com>
2020-08-20 14:42:50 +0200

    gcc-arm-none-eabi: update version date and sha256

--- Formula/gcc-arm-none-eabi.rb
+++ Formula/gcc-arm-none-eabi.rb
@@ -4,8 +4,8 @@ class GccArmNoneEabi < Formula
   desc "GNU Embedded Toolchain for ARM"
   homepage "https://developer.arm.com/open-source/gnu-toolchain/gnu-rm/downloads"
   url "https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2020q2/gcc-arm-none-eabi-9-2020-q2-update-mac.tar.bz2"
-  version "20191106"
-  sha256 "1249f860d4155d9c3ba8f30c19e7a88c5047923cea17e0d08e633f12408f01f0"
+  version "20200630"
+  sha256 "bbb9b87e442b426eca3148fa74705c66b49a63f148902a0ea46f676ec24f9ac6"
 
   def install
     ohai "Copying binaries..."
