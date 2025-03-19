7741a31c6034d574608e023a0563a16bef1ff9eb
Ramon Roche <mrpollo@gmail.com>
2025-03-19 13:28:54 -0700

    formula: update px4-sim-gazebo plus adds ci test

--- Formula/px4-sim-gazebo.rb
+++ Formula/px4-sim-gazebo.rb
@@ -12,22 +12,17 @@ class XRequirement < Requirement
 end
 
 class Px4SimGazebo < Formula
-  desc "PX4 Gazebo simulation"
+  desc "PX4 Gazebo Simulation Toolkit"
   homepage "http://px4.io"
   url "https://raw.githubusercontent.com/PX4/PX4-Autopilot/master/Tools/px4.py"
-  version "1.11.0"
+  version "1.15.0"
   sha256 "6e4a8a6aad592a0c156d16233e16ee276fd6cc63b620be5bd047375b1352e27d"
   depends_on "exiftool"
   depends_on "glog"
   depends_on "graphviz"
-  depends_on "gst-libav"
-  depends_on "gst-plugins-bad"
-  depends_on "gst-plugins-base"
-  depends_on "gst-plugins-good"
-  depends_on "gst-plugins-ugly"
   depends_on "gstreamer"
   depends_on "opencv"
-  depends_on "osrf/simulation/gazebo11"
+  depends_on "osrf/simulation/gz-harmonic"
   depends_on "protobuf"
   depends_on "px4-dev"
 
