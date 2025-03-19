30843775112d3e751ebed6116ecc9838bb55d8c3
Ramon Roche <mrpollo@gmail.com>
2025-03-19 14:45:00 -0700

    formula: fixes tools path
    Signed-off-by: Ramon Roche <mrpollo@gmail.com>

--- Formula/px4-sim-gazebo.rb
+++ Formula/px4-sim-gazebo.rb
@@ -14,7 +14,7 @@ end
 class Px4SimGazebo < Formula
   desc "PX4 Gazebo Simulation Toolkit"
   homepage "http://px4.io"
-  url "https://raw.githubusercontent.com/PX4/PX4-Autopilot/master/main/px4.py"
+  url "https://raw.githubusercontent.com/PX4/PX4-Autopilot/main/Tools/px4.py"
   version "1.15.0"
   sha256 "6e4a8a6aad592a0c156d16233e16ee276fd6cc63b620be5bd047375b1352e27d"
   depends_on "exiftool"
