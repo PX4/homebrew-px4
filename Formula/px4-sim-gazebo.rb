9eeae4a5b543e06d578cf4d3b494e4f57860a261
Ramon Roche <mrpollo@gmail.com>
2025-03-19 14:42:00 -0700

    formula: add simple test
    Signed-off-by: Ramon Roche <mrpollo@gmail.com>

--- Formula/px4-sim-gazebo.rb
+++ Formula/px4-sim-gazebo.rb
@@ -14,7 +14,7 @@ end
 class Px4SimGazebo < Formula
   desc "PX4 Gazebo Simulation Toolkit"
   homepage "http://px4.io"
-  url "https://raw.githubusercontent.com/PX4/PX4-Autopilot/master/Tools/px4.py"
+  url "https://raw.githubusercontent.com/PX4/PX4-Autopilot/master/main/px4.py"
   version "1.15.0"
   sha256 "6e4a8a6aad592a0c156d16233e16ee276fd6cc63b620be5bd047375b1352e27d"
   depends_on "exiftool"
@@ -29,6 +29,11 @@ class Px4SimGazebo < Formula
   def install
     mkdir_p "#{bin}/"
     cp "px4.py", "#{bin}/px4-sim-gazebo.py"
-    ohai "PX4 Gazebo simulation installed"
+    ohai "PX4 Gazebo Simulation Installed"
+  end
+
+  test do
+    output = shell_output("#{bin}/gz sim --version 2> /dev/null")
+    assert_match "Gazebo Sim, version 8*", output
   end
 end
