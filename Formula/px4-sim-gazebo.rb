a70b3c671a1d23fc09cb9b109b17a8465b14726e
Ramon Roche <mrpollo@gmail.com>
2025-04-28 12:18:06 -0700

    ci: change install formula

--- Formula/px4-sim-gazebo.rb
+++ Formula/px4-sim-gazebo.rb
@@ -16,7 +16,7 @@ class Px4SimGazebo < Formula
   homepage "http://px4.io"
   url "https://raw.githubusercontent.com/PX4/PX4-Autopilot/main/Tools/px4.py"
   version "1.15.0"
-  sha256 "6e4a8a6aad592a0c156d16233e16ee276fd6cc63b620be5bd047375b1352e27d"
+  sha256 "5eda2111dc20c092dc8241768121cf4173edabb593b2b199e6c233b5088c52aa"
   depends_on "exiftool"
   depends_on "glog"
   depends_on "graphviz"
