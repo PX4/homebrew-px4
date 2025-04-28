ddab18050c5ce044eea6499c6d26b8d2a92e76a7
Ramon Roche <mrpollo@gmail.com>
2025-04-28 15:25:36 -0700

    formula: px4-dev audit fixes
    Signed-off-by: Ramon Roche <mrpollo@gmail.com>

--- Formula/px4-dev.rb
+++ Formula/px4-dev.rb
@@ -1,6 +1,6 @@
 class Px4Dev < Formula
   desc "PX4 development toolchain"
-  homepage "http://px4.io"
+  homepage "https://px4.io"
   url "https://raw.githubusercontent.com/PX4/PX4-Autopilot/main/Tools/px4.py"
   version "1.15.0"
   sha256 "5eda2111dc20c092dc8241768121cf4173edabb593b2b199e6c233b5088c52aa"
