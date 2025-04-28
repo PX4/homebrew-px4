ee8c857eec9bb55a66a41605506b501e885152eb
Ramon Roche <mrpollo@gmail.com>
2025-04-28 13:25:44 -0700

    formula: fail silently if no match
    Signed-off-by: Ramon Roche <mrpollo@gmail.com>

--- Formula/fastdds.rb
+++ Formula/fastdds.rb
@@ -12,21 +12,21 @@ class Fastdds < Formula
   depends_on "foonathan-memory"
 
   def install
-    # Replace deprecated asio::io_service with asio::io_context throughout sources
+    # Silently replace deprecated asio::io_service with asio::io_context
     Dir.glob("include/fastdds/**/*.{h,cpp,hpp}").each do |file|
-      inreplace file, "asio::io_service", "asio::io_context"
+      inreplace file do |s|
+        s.gsub! "asio::io_service", "asio::io_context"
+      end
     end
 
-    # Out-of-tree build to avoid nested chdir conflicts
+    # Out-of-tree build to avoid nested Dir.chdir conflicts
     build_dir = buildpath/"build"
     build_dir.mkpath
 
-    # Configure with updated CMake policy
+    # Configure, build, and install with CMake policy minimum
     system "cmake", "-S", ".", "-B", build_dir,
            "-DCMAKE_POLICY_VERSION_MINIMUM=3.5",
            *std_cmake_args
-
-    # Build and install
     system "cmake", "--build", build_dir
     system "cmake", "--install", build_dir
   end
